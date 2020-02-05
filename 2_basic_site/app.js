f1b2ecab4956857f0fb3347cc426b620f54120e7
Arthur Kuivanen <arthurkuivanen@Arthurs-iMac.local>
2020-02-05 13:24:06 -0500

    changed fonts, created some better footer display based on changing screen size

--- 2_basic_site/app.js
+++ 2_basic_site/app.js
@@ -7,9 +7,13 @@ $(() => {
         if($(window).scrollTop() > 50){
             $('nav').css({'height':'100px','background-color':' rgba(6,66,115,.8)','filter':'drop-shadow(0 3px 3px #064273)'});
             $('.nav-item').css({'font-size':'22px'});
+            $('.logo-top').css({'opacity':'0'});
+            $('.logo-bottom').css({'opacity':'1'});
         }else{
             $('nav').css({'height':'150px','background-color':'transparent', 'filter':'none'});
             $('.nav-item').css({'font-size':'24px'});
+            $('.logo-top').css({'opacity':'1'});
+            $('.logo-bottom').css({'opacity':'0'});
         };
     })
 
